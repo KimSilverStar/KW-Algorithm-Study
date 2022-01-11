9a487bc5e090d70b39888a6468816ac91ee342a9
tellang <tellang@daum.net>
2022-01-12 02:52:47 +0900

    클리어 커밋
    자바 11로 컴파일 시

--- src/tellang/src/Week1/N2075/Main.java
+++ src/tellang/src/Week1/N2075/Main.java
@@ -1,5 +1,7 @@
 package Week1.N2075;
 
+import static java.lang.Integer.parseInt;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -12,7 +14,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         var br = new BufferedReader(new InputStreamReader(System.in));
-        var N = Integer.parseInt(br.readLine());
+        var N = parseInt(br.readLine());
         List<Stack<Integer>> stacks = new ArrayList<>();
         for (int i = 0; i < N; i++) {
             stacks.add(new Stack<>());
@@ -20,7 +22,7 @@ public class Main {
         for (int i = 0; i < N; i++) {
             var st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                stacks.get(j).push(Integer.parseInt(st.nextToken()));
+                stacks.get(j).push(parseInt(st.nextToken()));
             }
         }
         var answer = 0;
