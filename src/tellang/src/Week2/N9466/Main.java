220795db1d87a3e1ff1e2d219815e5b1b714ba01
tellang <tellang@daum.net>
2022-01-17 15:40:20 +0900

    클리어 커밋

--- src/tellang/src/Week2/N9466/Main.java
+++ src/tellang/src/Week2/N9466/Main.java
@@ -19,29 +19,29 @@ public class Main {
             var n = parseInt(br.readLine());
             var st = new StringTokenizer(br.readLine());
             var list = new ArrayList<Integer>();
-            var isClear = new boolean[n + 1];
+            var nthVisited = new int[n + 1];
+            var step = new int[n + 1];
             list.add(-1);
             for (int i = 1; i <= n; i++) {
                 list.add(parseInt(st.nextToken()));
             }
             var result = n;
             for (int i = 1; i <= n; i++) {
-                if (isClear[i])
+                if (nthVisited[i] > 0)
                     continue;
-                var visited = new boolean[n + 1];
                 var stack = new Stack<Integer>();
                 stack.push(i);
-                while (!stack.isEmpty()) {
+                for (int j = 1; !stack.isEmpty(); j++) {
                     var pop = stack.pop();
-                    visited[pop] = true;
+                    nthVisited[pop] = j;
+                    step[pop] = i;
                     var next = list.get(pop);
-                    if (!isClear[next]) {
-                        if (visited[next]) {
-                            result--;
-                            isClear[next] = true;
-                        }
+                    if (nthVisited[next] > 0) {
+                        if (step[next] == i)
+                            result -= (nthVisited[pop] - nthVisited[next] + 1);
+                        break;
+                    } else
                         stack.push(next);
-                    }
                 }
             }
             sb.append(result).append('\n');
