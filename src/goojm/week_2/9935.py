0b00ce0bc0c8396b54f8886cf28f134e2aa577a5
GoojungMyeon <tmd0707@naver.com>
2022-01-19 12:14:12 +0900

    add week_2
    add week_2

---
+++ src/goojm/week_2/9935.py
@@ -0,0 +1,95 @@
+import sys
+input = sys.stdin.readline
+
+prob_str = input().rstrip()
+bomb_str = input().rstrip()
+except_case = "FRULA"
+
+if len(prob_str) < len(bomb_str):
+    print(prob_str)
+else:
+    done = True
+    point = 0
+    while done:
+        for ind in range(point, len(prob_str)):
+            if ind+len(bomb_str) > len(prob_str):
+                done = False
+                break
+            if prob_str[ind:ind+len(bomb_str)] == bomb_str:
+                prob_str = prob_str[:ind]+prob_str[ind+len(bomb_str):]
+                point = ind - len(bomb_str) if ind - len(bomb_str) >= 0 else 0
+                break
+            if ind+1 == len(prob_str):
+                done = False
+        if len(prob_str) == 0:
+            done = False
+    if len(prob_str) == 0:
+        print(except_case)
+    else:
+        print(prob_str)
+
+
+# def div_cq(targetlst, bomb):
+#     hapcount = 0
+#     n_tlst = []
+#     hap = ""
+#     for t_ind in range(len(targetlst)):
+#         target = targetlst[t_ind]
+#         ddone = True
+#         while ddone:
+#             if len(target) == 0:
+#                 ddone = False
+#             for ind in range(len(target)):
+#                 if ind+len(bomb) > len(target):
+#                     ddone = False
+#                     break
+#                 if target[ind:ind+len(bomb)] == bomb:
+#                     target = target[:ind]+target[ind+len(bomb):]
+#                     break
+#                 if ind+1 == len(target):
+#                     ddone = False
+#         if hapcount == 1 and len(target) != 0:
+#             n_tlst.append(hap+target)
+#             hap = ""
+#             hapcount = 0
+#         elif hapcount == 0 and len(target) != 0:
+#             hap += target
+#             hapcount = 1
+#             if t_ind+1 == len(targetlst):
+#                 n_tlst.append(hap)
+#                 hap = ""
+#     if hap != "":
+#         n_tlst.append(hap)
+#     return n_tlst
+
+
+# prob_str = input().rstrip()
+# bomb_str = input().rstrip()
+# except_case = "FRULA"
+
+# if len(prob_str) < len(bomb_str):
+#     print(prob_str)
+# else:
+#     tlst = []
+#     for s_ind in range(int(len(prob_str)/len(bomb_str))):
+#         tlst.append(prob_str[s_ind*len(bomb_str):(s_ind+1)*len(bomb_str)])
+#     if len(prob_str) % len(bomb_str) != 0:
+#         tlst.append(prob_str[(s_ind+1)*len(bomb_str):])
+#     print(tlst)
+#     done = True
+#     f_count = 0
+#     while done:
+#         tlst = div_cq(tlst, bomb_str)
+#         if len(tlst) == 0 or (len(tlst) == 1 and f_count == 1):
+#             done = False
+#         if f_count == 0 and len(tlst) == 1:
+#             f_count = 1
+
+#     if len(tlst) == 0:
+#         print(except_case)
+#     else:
+#         print(tlst[0])
+
+
+# prob_str = "121212ababab"
+# bomb_str = "12ab"
