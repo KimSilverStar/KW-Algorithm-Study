98177f8b743518c7c9c3acf134c8fd815b96857f
tellang <tellang@daum.net>
2022-01-20 14:53:51 +0900

    클리어 커밋

--- src/tellang/src/Week2/N9935/Main.java
+++ src/tellang/src/Week2/N9935/Main.java
@@ -3,37 +3,24 @@ package Week2.N9935;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         var br = new BufferedReader(new InputStreamReader(System.in));
-        var sb = new StringBuilder(br.readLine());
+        var ori = br.readLine();
+        var sb = new StringBuilder();
         var target = br.readLine();
-        var stack = new Stack<Integer>();
         var index = 0;
         var size = target.length();
-        while (index <= sb.length() - size) {
-            while (sb.charAt(index) == target.charAt(0)) {
-                if (sb.substring(index, index + size).equals(target))
-                    sb.delete(index, index + size);
-                else if (sb.charAt(index + 1) == target.charAt(0))
-                    stack.push(index++);
-                else
-                    index++;
-                if (index > sb.length() - size || sb.isEmpty())
-                    break;
+        while (index < ori.length()) {
+            sb.append(ori.charAt(index));
+            if (sb.length() >= size && sb.charAt(sb.length() - 1) == target.charAt(size - 1)) {
+                if (sb.substring(sb.length() - size).equals(target)) {
+                    sb.delete(sb.length() - size, sb.length());
+                }
             }
             index++;
-        }
-        while (!stack.isEmpty()) {
-            var pop = stack.pop();
-            if (sb.length() < pop + size)
-                continue;
-            if (sb.substring(pop, pop + size).equals(target)) {
-                sb.delete(pop, pop + size);
-            }
         }
         if (sb.isEmpty())
             sb.append("FRULA");
