72aa5b7d981d2d71b57c454a8ed4b0d2d4125083
Oneul <toda1213@naver.com>
2022-01-28 14:57:52 +0900

    Update 1600.py
    메모리초과

--- src/oneul/1600.py
+++ src/oneul/1600.py
@@ -1,4 +1,5 @@
-# 6퍼 틀렸습니다
+# 6퍼 메모리초과
+# 아래 반례는 해결
 # 반례
 # 1
 # 5 3
@@ -9,7 +10,6 @@
 # 출력 : -1
 import sys
 from collections import deque
-
 
 si = sys.stdin.readline
 k = int(si().rstrip())
@@ -26,15 +26,18 @@ def in_range(x, y):
 def is_min_step(x, y, step):
     return answer[x][y] == -1 or answer[x][y] > step
 
-def can_go(x, y, step):
-    # 범위를 벗어나지 않았고 지금까지 최소 동작으로 가는 것일 때
-    return in_range(x, y) and is_min_step(x, y, step) and grid[x][y] != 1
+def can_go(x, y, visited):
+    # 범위를 벗어나지 않았고 지금 경로에서 방문하지 않았을 때
+    return in_range(x, y) and (x, y) not in visited and grid[x][y] != 1
 
 def bfs():
     global answer, k
     start_x, start_y, start_step = 0, 0, 0
     q = deque()
-    q.append((start_x, start_y, start_step, k))
+    visited = set()
+
+    visited.add((start_x, start_y))
+    q.append((start_x, start_y, start_step, k, set(visited)))
     answer[start_x][start_y] = 0
 
     dxs_monkey, dys_monkey = [ -1, 1, 0, 0 ], [ 0, 0, -1, 1 ]
@@ -42,19 +45,25 @@ def bfs():
     dys_horse = [ -2, -1, 1, 2, 2, 1, -1, -2 ]
 
     while q:
-        x, y, step, rem_k = q.popleft()
+        x, y, step, rem_k, curr_visited = q.popleft()
 
         for dx, dy in zip(dxs_monkey, dys_monkey):
             new_x, new_y, next_step = x + dx, y + dy, step + 1
-            if can_go(new_x, new_y, next_step):
-                q.append((new_x, new_y, next_step, rem_k))
-                answer[new_x][new_y] = next_step
+            if can_go(new_x, new_y, curr_visited):
+                curr_visited.add((new_x, new_y))
+                q.append((new_x, new_y, next_step, rem_k, set(curr_visited)))
+                curr_visited.remove((new_x, new_y))
+                if is_min_step(new_x, new_y, next_step):
+                    answer[new_x][new_y] = next_step
         
         for dx, dy in zip(dxs_horse, dys_horse):
             new_x, new_y, next_step = x + dx, y + dy, step + 1
-            if can_go(new_x, new_y, next_step) and rem_k:
-                q.append((new_x, new_y, next_step, rem_k - 1))
-                answer[new_x][new_y] = next_step
+            if can_go(new_x, new_y, curr_visited) and rem_k:
+                curr_visited.add((new_x, new_y))
+                q.append((new_x, new_y, next_step, rem_k - 1, set(curr_visited)))
+                curr_visited.remove((new_x, new_y))
+                if is_min_step(new_x, new_y, next_step):
+                    answer[new_x][new_y] = next_step
 
 
 bfs()
