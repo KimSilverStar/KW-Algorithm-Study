7a92e75485f0f5134f386cfb95cfda18c61b366a
tellang <tellang@daum.net>
2022-02-15 16:33:21 +0900

    클리어 커밋

--- src/tellang/src/Week4/N21758/Main.java
+++ src/tellang/src/Week4/N21758/Main.java
@@ -14,22 +14,38 @@ public class Main {
         var N = parseInt(br.readLine());
         var st = new StringTokenizer(br.readLine());
         var arr = new int[N];
+        var left2Right = new int[N];
+        var right2Left = new int[N];
         var max = 0;
         var total = 0;
         var pillar = 0;
+
         for (int i = 0; i < N; i++) {
             arr[i] = parseInt(st.nextToken());
             total += arr[i];
-            if (i > 0 && i < N - 1 && pillar < arr[i])
+            if (i > 0 && i < N - 1 && pillar < arr[i]) {
                 pillar = arr[i];
+                left2Right[i] += (left2Right[i - 1] + arr[i]);
+            } else {
+                if (i == 0)
+                    left2Right[i] = arr[i];
+                else
+                    left2Right[i] += (left2Right[i - 1] + arr[i]);
+            }
         }
+
+        right2Left[N - 1] = arr[N - 1];
+        for (int i = N - 2; i >= 0; i--) {
+            right2Left[i] += right2Left[i + 1] + arr[i];
+        }
+
         for (int i = 1; i < N - 1; i++) {
-            var rightHorny = total - arr[0] - arr[i] + lineIntegration(arr, i, N - 1);
+            var rightHorny = total - arr[0] - arr[i] + right2Left[i + 1];
             if (rightHorny > max)
                 max = rightHorny;
         }
         for (int i = N - 2; i > 0; i--) {
-            var leftHorny = total - arr[N - 1] - arr[i] + lineIntegration(arr, -1, i - 1);
+            var leftHorny = total - arr[N - 1] - arr[i] + left2Right[i - 1];
             if (leftHorny > max)
                 max = leftHorny;
         }
